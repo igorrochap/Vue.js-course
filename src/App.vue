a45ce0c60dabfaab38519fbf99b27dadcd539586
Igor <mial>
2020-10-02 07:29:12 -0300

    Aula 14 - V-for

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    
+    <div v-for="(cliente, index) in clientes" :key="cliente.id">
+      <h4>{{ index }}</h4>
+      <hr>
+      <Cliente 
+        :cliente="cliente"
+        :showAge="true"
+      />
+      <hr>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
+  </div>
+</template>
+
+<script>
+import Cliente from './components/Cliente';
+//import Produto from './components/Produto';
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      clientes: [
+        {
+          id: 1,
+          nome: 'Victor Lima',
+          email: 'victor@lima.com',
+          idade: 99
+        },
+        {
+          id: 2,
+          nome: 'Igor Rocha',
+          email: 'igor@rocha.com',
+          idade: 20
+        },
+        {
+          id: 3,
+          nome: 'João Jorge',
+          email: 'joao@jorge.com',
+          idade: 77
+        },
+        {
+          id: 4,
+          nome: 'Willian Butcher',
+          email: 'billy@butcher.com',
+          idade: 40
+        },
+        {
+          id: 5,
+          nome: 'Hughie Campbell',
+          email: 'hughie@campbell.com',
+          idade: 28
+        }
+      ]
+    }
+  },
+  components: {
+    Cliente,
+    //Produto
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,54 @@
+<template>
+    <div :class="{'cliente' : !isPremium, 'cliente-premium' : isPremium}">
+        <h4>Nome: {{ cliente.nome }}</h4>
+        <hr>
+        <p>Email: {{ cliente.email }}</p>
+        <!--<p v-show="showAge === true">Idade: {{ cliente.idade }}</p>-->
+        <p v-if="showAge === true">Idade: {{ cliente.idade }}</p>
+        <p v-else>A idade do usuário é privada</p>
+
+        <button v-on:click="isPremium = true">Premium</button>
+        <button v-on:click="isPremium = false">Normal</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                /*numero: "1234",
+                email: "jorge1234@jorge.com",
+                descricao: "Descrição",
+                idade: 77*/
+                isPremium: false,
+            }
+        },
+        props: {
+            //nome: String,
+            //numero: String,
+            //email: String,
+            //descricao: String,
+            //idade: Number,
+            cliente: Object,
+            showAge: Boolean
+        }
+    }
+</script>
+
+<style scoped>
+    .cliente {
+        background-color: rgb(177, 177, 177);    
+        max-width: 600px;
+        height: 180px;
+        padding: 2%;
+        margin-top: 2%;
+    }
+
+    .cliente-premium{
+        background-color: rgb(255, 166, 0);    
+        max-width: 600px;
+        height: 180px;
+        padding: 2%;
+        margin-top: 2%;
+    }
+</style>
